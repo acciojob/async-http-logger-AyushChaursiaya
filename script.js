4be1d8ddac2c13b648ab731926930426eba27455
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-11 11:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const urls = 'https://jsonplaceholder.typicode.com/todos/1';
+
+async function fetchData() {
+	try{	
+		const response = await fetch(urls);
+		const data = await response.json();
+		console.log(data);
+	}catch(error) {
+		console.log(error);
+	}
+}
+fetchData();
